c49b4811250d01d74cf8db91564c1ab5937f6941
Mike Krinkin <krinkin.m.u@gmail.com>
2015-01-15 21:29:08 +0300

    add e820 memory map detection

--- memory.c
+++ memory.c
@@ -1,11 +1,20 @@
 #include "boot.h"
+#include "printf.h"
 
-#define SMAP 0x534D4150
+#define SMAP             0x534D4150
+#define RAM              1
+#define RESERVED         2
+#define ACPI_RECLAIMABLE 3
+#define ACPI_NVS         4
+#define BAD_MEMORY       5
 
-static int e820_detect(struct e820entry *entries, unsigned size)
+
+int e820_detect(struct boot_params *params)
 {
 	struct biosregs iregs, oregs;
 	struct e820entry buf;
+	struct e820entry *entries = params->e820_mm;
+	const int size = sizeof(params->e820_mm) / sizeof(buf);
 	int count = 0;
 
 	initregs(&iregs);
@@ -20,19 +29,42 @@ static int e820_detect(struct e820entry *entries, unsigned size)
 		if (oregs.eflags & X86_CF_FLAG)
 			break;
 
-		if (oregs.eax != SMAP)
-			return 0;
+		if (oregs.eax != SMAP) {
+			count = 0;
+			break;
+		}
 
 		iregs.ebx = oregs.ebx;
-		entries[count] = buf;
-		count++;
+		entries[count++] = buf;
 	} while (iregs.ebx && count < size);
 
+	params->e820_entries = count;
 	return count;
 }
 
-int detect_memory(struct boot_params *params)
+static const char *e820_type(unsigned long type)
 {
-	params->e820_entries = e820_detect(params->e820_mm, E820_MAX_MAP_SIZE);
-	return params->e820_entries;
+	switch (type) {
+	case RAM: return "RAM";
+	case RESERVED: return "reserved";
+	case ACPI_RECLAIMABLE: return "ACPI reclaimable memory";
+	case ACPI_NVS: return "ACPI NVS";
+
+	case BAD_MEMORY:
+	default: return "unknown";
+	}
+}
+
+void e820_show(const struct boot_params *params)
+{
+	const struct e820entry *entries = params->e820_mm;
+	int i;
+
+	printf("Memory map (%u ranges):\n", params->e820_entries);
+	for (i = 0; i < params->e820_entries; ++i) {
+		printf("region 0x%x-0x%x %s\n",
+			entries[i].addrl,
+			entries[i].addrl + entries[i].sizel - 1,
+			e820_type(entries[i].type));
+	}
 }
