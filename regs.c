09dcbd23079bae30aa658434df6392daabc9fc0c
Mike Krinkin <krinkin.m.u@gmail.com>
2015-01-15 14:01:31 +0300

    generic BIOS interrupt function (from Linux)

---
+++ regs.c
@@ -0,0 +1,12 @@
+#include "boot.h"
+#include "string.h"
+
+void initregs(struct biosregs *regs)
+{
+	memset(regs, 0, sizeof(*regs));
+	regs->eflags |= X86_CF_FLAG;
+	regs->ds = get_ds();
+	regs->es = get_es();
+	regs->fs = get_fs();
+	regs->gs = get_gs();
+}
