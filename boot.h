980598e8f1a3b8863a847152da71635c41f41b9b
Mike Krinkin <krinkin.m.u@gmail.com>
2015-01-17 19:51:42 +0300

    utility port and memory access functions

--- boot.h
+++ boot.h
@@ -4,8 +4,6 @@
 #define E820_MAX_MAP_SIZE 128
 #define X86_CF_FLAG (1 << 0)
 
-void die(void) __attribute__((noreturn));
-
 struct boot_header {
 	unsigned long magic;
 	unsigned long image_size;
@@ -73,32 +71,135 @@ struct biosregs {
 	};
 };
 
+static inline __attribute__((noreturn)) void die(void)
+{
+	while (1)
+		asm("hlt");
+}
+
+static inline void cli(void)
+{
+	asm volatile("cli");
+}
+
+static inline void sti(void)
+{
+	asm volatile("sti");
+}
+
+static inline void io_delay(void)
+{
+	/* it is safe to use 0x80 port */
+	asm volatile("outb %al, $0x80");
+}
+
+static inline void outb(unsigned char v, unsigned short port)
+{
+	asm volatile("movb %0, %%al; outb %%al, %1"
+			:
+			: "i"(v), "i"(port)
+			: "al");
+}
+
+static inline unsigned char inb(unsigned short port)
+{
+	unsigned char v;
+
+	asm volatile("inb %1, %%al; movb %%al, %0"
+			: "=rm"(v)
+			: "i"(port)
+			: "al");
+	return v;
+}
+
+static inline void outw(unsigned short v, unsigned short port)
+{
+	asm volatile("movw %0, %%ax; outw %%ax, %1"
+			:
+			: "i"(v), "i"(port)
+			: "ax");
+}
+
+static inline unsigned short inw(unsigned short port)
+{
+	unsigned short v;
+
+	asm volatile("inw %1, %%ax; movw %%ax, %0"
+			: "=rm"(v)
+			: "i"(port)
+			: "ax");
+	return v;
+}
+
+static inline void outl(unsigned long v, unsigned short port)
+{
+	asm volatile("movl %0, %%eax; outl %%eax, %1"
+			:
+			: "i"(v), "i"(port)
+			: "eax");
+}
+
+static inline unsigned long inl(unsigned short port)
+{
+	unsigned long v;
+
+	asm volatile("inl %1, %%eax; movl %%eax, %0"
+			: "=rm"(v)
+			: "i"(port)
+			: "eax");
+	return v;
+}
+
 static inline unsigned short get_ds(void)
 {
 	unsigned short seg;
-	asm("movw %%ds,%0" : "=rm"(seg));
-	return seg;
-}
 
-static inline unsigned short get_fs(void)
-{
-	unsigned short seg;
-	asm("movw %%fs,%0" : "=rm"(seg));
-	return seg;
-}
-
-static inline unsigned short get_gs(void)
-{
-	unsigned short seg;
-	asm("movw %%gs,%0" : "=rm"(seg));
+	asm volatile("movw %%ds,%0" : "=rm"(seg));
 	return seg;
 }
 
 static inline unsigned short get_es(void)
 {
 	unsigned short seg;
-	asm("movw %%es,%0" : "=rm"(seg));
+
+	asm volatile("movw %%es,%0" : "=rm"(seg));
 	return seg;
+}
+
+static inline void set_fs(unsigned short seg)
+{
+	asm volatile("movw %0,%%fs" : : "rm"(seg));
+}
+
+static inline void set_gs(unsigned short seg)
+{
+	asm volatile("movw %0,%%gs" : : "rm"(seg));
+}
+
+static inline unsigned long readl_gs(unsigned short off)
+{
+	unsigned long v;
+
+	asm volatile("movl %%gs:%1,%0" : "=q"(v) : "m"(off));
+	return v;
+}
+
+static inline void writel_gs(unsigned short off, unsigned long v)
+{
+	asm volatile("movl %0,%%gs:%1" : : "qi"(v), "m"(off));
+}
+
+static inline unsigned long readl_fs(unsigned short off)
+{
+	unsigned long v;
+
+	asm volatile("movl %%fs:%1,%0" : "=q"(v) : "m"(off));
+	return v;
+}
+
+static inline void writel_fs(unsigned short off, unsigned long v)
+{
+	asm volatile("movl %0,%%fs:%1" : : "qi"(v), "m"(off));
 }
 
 void initregs(struct biosregs *ireg);
@@ -106,5 +207,7 @@ void intcall(int intno, const struct biosregs *ireg, struct biosregs *oreg);
 
 int e820_detect(struct boot_params *params);
 void e820_show(const struct boot_params *params);
+
+int enable_a20(void);
 
 #endif /*__BOOT_H__*/
